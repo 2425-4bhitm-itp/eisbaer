6dfe50f812320e644068744defe915b1efba0630
“michael-stuetzner” <m.stuetzner@students.htl-leonding.ac.at>
2024-10-24 12:39:58 +0200

    moved: code from old js in new js

--- backend/src/main/resources/META-INF/resources/script.js
+++ backend/src/main/resources/META-INF/resources/script.js
@@ -1,11 +1,20 @@
-const url = "http://localhost:8080/api/articles/"
-let id = document.getElementById("userInput").innerText;
+const url = "http://localhost:8080/Articles/getArticle/"
+let idInput = document.getElementById("userInput");
+let output = document.getElementById("outputPlace");
 
 function getArticlePosition() {
+    let id = idInput.value;
+    console.log(id)
     fetch(url + id, {
         method: 'GET',
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        .then(response => response.text())
+        .then((response) => {
+            showArticlePosition(response);
+        })
         .catch(err => console.error(err));
+}
+
+function showArticlePosition(position) {
+    output.innerText = position;
 }
