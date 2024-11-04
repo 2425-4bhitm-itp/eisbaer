68dc034295886529594fb0426ba90dc73bca3465
ebrandstaetter <e.brandstaetter@students.htl-leonding.ac.at>
2024-11-04 15:28:28 +0100

    updated: 3d model import

--- backend/src/main/resources/META-INF/resources/3dModel.js
+++ backend/src/main/resources/META-INF/resources/3dModel.js
@@ -2,5 +2,19 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const loader = new GLTFLoader();
+loader.load( './3d/eisbaer.glb',
+    function ( gltf ) { scene.add( gltf.scene ); },
+    undefined,
+    function ( error ) { console.error( error );}
+);
+
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
