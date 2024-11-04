547816bd2a7fb46a6832e0fa76c70e64a110841a
ebrandstaetter <e.brandstaetter@students.htl-leonding.ac.at>
2024-11-04 15:07:38 +0100

    added: base files for displaying of 3D-Model (ThreeJS)

---
+++ backend/src/main/resources/META-INF/resources/3dModel.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
