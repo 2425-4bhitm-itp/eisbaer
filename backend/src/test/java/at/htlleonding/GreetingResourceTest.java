959f71c0dce624f4093b2e5e1360d56f695b3444
JakobMichel <jakob.michel25@gmail.com>
2024-11-07 12:33:21 +0100

    feature #21: implemented opensearch with docker on quarkus startup

--- backend/src/test/java/at/htlleonding/GreetingResourceTest.java
+++ backend/src/test/java/at/htlleonding/GreetingResourceTest.java
@@ -8,13 +8,6 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class GreetingResourceTest {
-    @Test
-    void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+
 
 }
