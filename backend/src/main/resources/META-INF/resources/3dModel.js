d804a64a527025065665145dc67f181424d2d35a
ebrandstaetter <4.brandst.e@gmail.com>
2024-11-11 09:39:32 +0100

    changed: fixed ThreeJS window size

--- backend/src/main/resources/META-INF/resources/3dModel.js
+++ backend/src/main/resources/META-INF/resources/3dModel.js
@@ -2,32 +2,35 @@ import * as THREE from 'three';
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'jsm/loaders/GLTFLoader.js';
 
+let avatarContainer = document.getElementById('avatarContainer');
+const w = avatarContainer.offsetWidth;
+const h = avatarContainer.offsetHeight;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.x = 5;
 camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+avatarContainer.appendChild(renderer.domElement);
 
-const light = new THREE.AmbientLight( 0xffffff );
-scene.add( light );
+const globalLight = new THREE.AmbientLight(0x404040, 1);
+scene.add(globalLight);
 
-var bear;
+var bearGLB;
 
 const loader = new GLTFLoader();
 loader.load('./eisbaer.glb', function (glb) {
-    bear = glb.scene;
-    scene.add(bear);
+    bearGLB = glb.scene;
+    scene.add(bearGLB);
 }, undefined, function (error) {
     console.error(error);
 });
 
 function animate() {
-    if (bear) {
-        bear.rotation.y += 0.008;
+    if (bearGLB) {
+        bearGLB.rotation.y += 0.008;
     }
     renderer.render( scene, camera );
 }
