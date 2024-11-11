622020f17119cc341166bab187ec26e19ae883dc
ebrandstaetter <4.brandst.e@gmail.com>
2024-11-11 09:35:13 +0100

    changed: reworked ThreeJS Code, should work now

--- backend/src/main/resources/META-INF/resources/3dModel.js
+++ backend/src/main/resources/META-INF/resources/3dModel.js
@@ -2,36 +2,33 @@ import * as THREE from 'three';
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'jsm/loaders/GLTFLoader.js';
 
-let avatarContainer = document.getElementById('avatarContainer');
-const w = avatarContainer.offsetWidth;
-const h = avatarContainer.offsetHeight;
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(w, h);
-avatarContainer.appendChild(renderer.domElement);
-
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 2;
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xcccccc);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.x = 5;
+camera.lookAt(0, 0, 0);
 
-const globalLight = new THREE.AmbientLight(0x404040, 1);
-scene.add(globalLight);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
+
+var bear;
 
 const loader = new GLTFLoader();
-loader.load('./3d/eisbaer.gltf', function (gltf) {
-    scene.add(gltf.scene);
+loader.load('./eisbaer.glb', function (glb) {
+    bear = glb.scene;
+    scene.add(bear);
 }, undefined, function (error) {
     console.error(error);
 });
 
-const controls = new OrbitControls(camera, renderer.domElement);
-
-// Animation loop to render continuously
 function animate() {
-    requestAnimationFrame(animate);
-    controls.update(); // Only necessary if controls.enableDamping or other dynamic settings are used
-    renderer.render(scene, camera);
+    if (bear) {
+        bear.rotation.y += 0.008;
+    }
+    renderer.render( scene, camera );
 }
-animate();
+renderer.setAnimationLoop( animate );
