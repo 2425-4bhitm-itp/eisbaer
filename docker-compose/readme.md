39f51d14fd82051bf05163ffd240eaf371e1990d
“michael-stuetzner” <m.stuetzner@students.htl-leonding.ac.at>
2024-11-27 08:22:06 +0100

    fixed #31: quarkus can't start error

---
+++ docker-compose/readme.md
@@ -0,0 +1,26 @@
+# Development using a local docker compose database
+
+## Starting
+
+```bash
+./start.sh
+```
+
+## delete all docker containers, images and volumes
+
+```bash
+./clean-docker.sh
+```
+
+
+## backup the database
+
+```bash
+docker exec -it postgres pg_dump -U demo > demo-backup.sql
+```
+
+## stop the database
+
+```bash
+./down.sh
+```
--- docker-compose/start.sh
+++ docker-compose/start.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+docker compose up --detach
+#docker compose logs postgres
+
+cat << EOF
+
+
+# ====== add the following to src/main/resources/quarkus.properties ======
 quarkus.datasource.db-kind=postgresql
 quarkus.datasource.username=demo
 quarkus.datasource.password=demo
@@ -7,12 +16,16 @@ quarkus.datasource.jdbc.url=jdbc:postgresql://localhost:5432/demo
 quarkus.hibernate-orm.database.generation=drop-and-create
 %prod.quarkus.hibernate-orm.database.generation=none
 quarkus.hibernate-orm.database.generation.halt-on-error=false
-%dev.quarkus.hibernate-orm.sql-load-script=insert.sql
+%dev.quarkus.hibernate-orm.sql-load-script=db/import.sql
 quarkus.datasource.devservices.enabled=false
 quarkus.hibernate-orm.log.sql=true
 quarkus.http.access-log.enabled=true
-
-quarkus.devservices.enabled=false
-
+# ======
 
 
+EOF
+
+echo "to stop the database enter the following:"
+echo "./down.sh"
+echo "to see the postgresql logs enter the following:"
+echo "docker compose logs -f postgres"
