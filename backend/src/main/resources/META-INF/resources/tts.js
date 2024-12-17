e1042e109b95f42ce61d7b16c816fdd46691e805
michael <m.stuetzner@students.htl-leonding.ac.at>
2024-12-17 08:33:08 +0100

    added #16: text to speech into project (bug: only works in firefox)

---
+++ backend/src/main/resources/META-INF/resources/tts.js
@@ -0,0 +1,33 @@
+const beginningText = document
+    .getElementsByClassName("transcriptText")[0]
+    .innerHTML;
+
+console.log("textToBeSpoken: " + beginningText);
+
+function speak(textToBeSpoken) {
+
+    if (!window.speechSynthesis) {
+        alert("Text-to-Speech wird von deinem Browser nicht unterstützt.");
+        return;
+    }
+
+    // Neue SpeechSynthesisUtterance erstellen
+    const utterance = new SpeechSynthesisUtterance(textToBeSpoken);
+
+    // Stimme und Sprache festlegen (z. B. Deutsch)
+    utterance.lang = "de-DE";
+
+    // Sprechgeschwindigkeit (1.0 ist Standard)
+    utterance.rate = 0.5;
+
+    utterance.pitch = 0.5;
+
+    // Text sprechen
+    window.speechSynthesis.speak(utterance);
+}
+
+function stopSpeech() {
+    window.speechSynthesis.cancel();
+}
+
+speak(beginningText);
