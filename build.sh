3ff155f45b7b29b56c9996da3a5ce5b3f4e4c1b0
michael <m.stuetzner@students.htl-leonding.ac.at>
2025-01-10 12:45:57 +0100

    changed: build.sh to do work of opensearch.http

--- build.sh
+++ build.sh
@@ -3,6 +3,59 @@ set -e
 pushd ./docker-compose
 . ./start.sh
 popd
+pushd ./backend/src/main/opensearch
+until curl -s -X GET "http://localhost:9200/_cluster/health?wait_for_status=green" | grep -q "green"; do
+  sleep 1
+  echo 'waiting...'
+done
+echo 'creating index...'
+curl -X PUT "http://localhost:9200/articles" -H "Content-Type: application/json" -d '{
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1
+  },
+  "mappings": {
+    "properties": {
+      "Bezeichnung1": {
+        "type": "text"
+      },
+      "Bezeichnung2": {
+        "type": "text"
+      },
+      "Laenge": {
+        "type": "integer"
+      },
+      "Breite": {
+        "type": "integer"
+      },
+      "Hoehe": {
+        "type": "integer"
+      },
+      "Durchmesser": {
+        "type": "integer"
+      },
+      "Lagerort": {
+        "type": "text"
+      },
+      "Lagerstand": {
+        "type": "float"
+      },
+      "LagereinheitBez": {
+        "type": "keyword"
+      },
+      "Stellplatz": {
+        "type": "text"
+      }
+    }
+  }
+}'
+echo 'index created'
+echo 'inserting bulk data...'
+curl -X POST "http://localhost:9200/articles/_bulk" -H 'Content-Type: application/json' --data-binary @bulk_data
+echo 'bulk data inserted'
+echo 'searching data for GET...'
+curl -X GET "http://localhost:9200/articles/_search?pretty=true&q=*"
+popd
 pushd ./backend
 mvn clean quarkus:dev
 popd
