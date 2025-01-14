3057ed02edce036bf5252e0f525c2539ca3f83f0
michael <m.stuetzner@students.htl-leonding.ac.at>
2025-01-14 08:47:35 +0100

    added: new tts version (responsiveVoice)

--- backend/src/main/resources/META-INF/resources/tts.js
+++ backend/src/main/resources/META-INF/resources/tts.js
@@ -1,48 +1,26 @@
 let checkIfFirstTime = true;
 
 const beginningText = document
-    .getElementsByClassName("transcriptText")[0]
-    .innerHTML;
+     .getElementsByClassName("transcriptText")[0]
+     .innerHTML;
 
 console.log("textToBeSpoken: " + beginningText);
 
 //Log all available voices
-const voices = window.speechSynthesis.getVoices();
-console.log(voices);
+var voicelist = responsiveVoice.getVoices();
+console.log(voicelist);
+
+//Set voice
+responsiveVoice.setDefaultVoice("Deutsch Male");
 
 function speak(textToBeSpoken) {
-
-    // Check if the browser supports the SpeechSynthesis API
-    if (!window.speechSynthesis) {
-        alert("Text-to-Speech wird von deinem Browser nicht unterstützt.");
-        return;
+        responsiveVoice.speak(textToBeSpoken);
+        responsiveVoice.cancel();
     }
-
-    // Create a new instance of SpeechSynthesisUtterance
-    const utterance = new SpeechSynthesisUtterance(textToBeSpoken);
-
-    // Set language to German
-    utterance.lang = "de-DE";
-
-    // Speaking speed and pitch (standard: 1)
-    utterance.rate = 0.9;
-
-    utterance.pitch = 0.5;
-
-    // Set the voice
-    utterance.voice = voices[0];
-
-    // speak the text
-    window.speechSynthesis.speak(utterance);
-}
-
-function stopSpeech() {
-    window.speechSynthesis.cancel();
-}
 
 document.body.addEventListener("click", function() {
-    if(checkIfFirstTime) {
-        speak(beginningText);
-        checkIfFirstTime = false;
-    }
+     if(checkIfFirstTime) {
+         speak(beginningText);
+         checkIfFirstTime = false;
+     }
 });
