1b9cd6d1b5bf2182735326706504516c3e7cded7
JakobMichel <jakob.michel25@gmail.com>
2025-01-20 09:58:45 +0100

    switching between talking avatar and temp image

--- backend/src/main/resources/META-INF/resources/switch2DAvatar.js
+++ backend/src/main/resources/META-INF/resources/switch2DAvatar.js
@@ -1,12 +1,18 @@
-let img = Document.getElementById("2dAvatar");
+const img = document.getElementById("2dAvatar");
 
 var loop= setInterval(switchAvatar,100);
+var started = true;
+
 console.log("loopstart ");
 
 function switchAvatar() {
-    if (isSpeaking) {
-        img.src = "images/2dAvatarSpeaking.gif";
-    } else {
+    if (isSpeaking && !started) {
+        started = true;
+        img.src = "./video/seamless-loop-talking-unscreen.gif";
+    }
+
+    if (!isSpeaking) {
+        started = false;
         img.src = "https://www.w3schools.com/js/pic_bulbon.gif";
     }
     console.log("isSpeaking: " + isSpeaking);
