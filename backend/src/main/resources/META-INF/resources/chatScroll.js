adcb443b91798a3fd53fd2731daddb55bda9346e
ebrandstaetter <4.brandst.e@gmail.com>
2025-02-03 14:32:38 +0100

    changed #40: fixed styling for portrait mode

--- backend/src/main/resources/META-INF/resources/chatScroll.js
+++ backend/src/main/resources/META-INF/resources/chatScroll.js
@@ -1,12 +1,22 @@
 // Get a reference to the div you want to auto-scroll.
 var transcriptContainer = document.querySelector('#transcriptContainer');
 
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
 // First, define a helper function.
 function animateScroll(duration) {
+
     var start = transcriptContainer.scrollTop;
     var end = transcriptContainer.scrollHeight - transcriptContainer.clientHeight;
     var change = end - start;
     var increment = 20;
+
+    //if window is in portrait mode, div will scroll to top
+    // if (windowHeight >= windowWidth) { todo: implement reverse scroll
+    //     change = start - end;
+    // }
+
     function easeInOut(currentTime, start, change, duration) {
         // by Robert Penner
         currentTime /= duration / 2;
@@ -29,7 +39,7 @@ function animateScroll(duration) {
     animate(0);
 }
 // Here's our main callback function we passed to the observer
-function scrollToBottom() {
+function chatScroll() {
     var duration = 300 // Or however many milliseconds you want to scroll to last
     animateScroll(duration);
 }
