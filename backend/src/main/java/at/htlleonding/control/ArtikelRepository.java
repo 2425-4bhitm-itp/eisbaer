f4d086694920f5d276fcf0ab82ece99fdaa98ea5
KonstantinOeller <k.oeller@students.htl-leonding.ac.at>
2025-02-27 10:43:40 +0100

    fixed#44: casting error when searching for product name

--- backend/src/main/java/at/htlleonding/control/ArtikelRepository.java
+++ backend/src/main/java/at/htlleonding/control/ArtikelRepository.java
@@ -11,9 +11,17 @@ public class ArtikelRepository implements PanacheRepository<Artikel>{
 
     public List<Artikel> search(String searchTerm) {
         String likePattern = "%" + searchTerm + "%";
-        return find("FKArtikelid LIKE ?1 OR Bezeichnung1 LIKE ?1 OR Bezeichnung2 LIKE ?1 OR " +
-                        "Laenge LIKE ?1 OR Breite LIKE ?1 OR Hoehe LIKE ?1 OR Durchmesser LIKE ?1 OR " +
-                        "Lagerort LIKE ?1 OR Lagerstand LIKE ?1 OR Lagereinheitbez LIKE ?1 OR Stellplatz LIKE ?1",
+        return find("CONCAT(FKArtikelid, '') LIKE ?1 OR " +
+                        "Bezeichnung1 LIKE ?1 OR " +
+                        "Bezeichnung2 LIKE ?1 OR " +
+                        "CONCAT(Laenge, '') LIKE ?1 OR " +
+                        "CONCAT(Breite, '') LIKE ?1 OR " +
+                        "CONCAT(Hoehe, '') LIKE ?1 OR " +
+                        "CONCAT(Durchmesser, '') LIKE ?1 OR " +
+                        "Lagerort LIKE ?1 OR " +
+                        "CONCAT(Lagerstand, '') LIKE ?1 OR " +
+                        "LagereinheitBez LIKE ?1 OR " +
+                        "Stellplatz LIKE ?1",
                 likePattern).list();
     }
 }
