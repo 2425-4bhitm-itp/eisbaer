20bcfa489eb81e51c8f851d81b47e97beb5df15e
KonstantinOeller <k.oeller@students.htl-leonding.ac.at>
2025-04-01 23:49:51 +0200

    modified resource

--- backend/src/main/java/at/htlleonding/boundary/ArticleResource.java
+++ backend/src/main/java/at/htlleonding/boundary/ArticleResource.java
@@ -3,6 +3,7 @@ package at.htlleonding.boundary;
 import at.htlleonding.control.ArtikelRepository;
 import at.htlleonding.entity.Artikel;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -27,5 +28,16 @@ public class ArticleResource {
         List<Artikel> artikelList = artikelRepository.search(searchString);
         artikelList = artikelList.subList(0, Math.min(artikelList.size(), MAXIMUM_ARTIKEL_RETURN_COUNT));
         return Response.ok(artikelList).build();
+    }
+
+    @POST
+    @Path("/addArticle")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response addArticle(Artikel artikel) {
+        System.out.println("adding article: " + artikel);
+        artikelRepository.persist(artikel);
+        return Response.ok().build();
     }
 }
