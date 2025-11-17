c10e79cbc4087b5aae550527971771046aa855b8
KonstantinOeller <k.oeller@students.htl-leonding.ac.at>
2025-11-17 18:34:25 +0100

    changed:#41 frontend server proxy

--- frontend/webpack.config.mjs
+++ frontend/webpack.config.mjs
@@ -21,7 +21,7 @@ const config = {
         historyApiFallback: true,
         proxy: [
             {
-                context: ['/search', '/eisbaer_rag_data'],
+                context: ["/eisbaer_rag_data", "/search"],
                 target: 'http://localhost:9200',
                 changeOrigin: true,
                 pathRewrite: { '^/search': '' }
